5852fad440471cb316a4326a31adcdb3a3116f39
ben <bjelinek42@gmail.com>
2022-03-13 17:32:34 -0600

    create controller

--- app/controllers/sandwhiches_controller.rb
+++ app/controllers/sandwhiches_controller.rb
@@ -6,9 +6,14 @@ class SandwhichesController < ApplicationController
   end
 
 
-def show
-  sandwhiches = Sandwhich.find_by(id: params[:id])
-  render json: sandwhiches
-end
+  def show
+    sandwhiches = Sandwhich.find_by(id: params[:id])
+    render json: sandwhiches
+  end
 
+  def create
+    sandwhich = Sandwhich.new({name: params["name"], price: params["price"], size: params["price"]})
+    sandwhich.save
+    render json: sandwhich
+  end
 end
